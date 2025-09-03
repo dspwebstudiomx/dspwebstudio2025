e3b3df1b0f5e0c807c70605cc60a10128aa7fef6
Daniel Perez <dspwebstudio@gmail.com>
2025-09-03 08:34:54 -0600

    Actualizar clase de Hero para ajustar el margen superior

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -9,7 +9,7 @@ export function Welcome() {
 			<Hero
 				heroId="main"
 				backgroundImage="https://images.unsplash.com/photo-1644088379091-d574269d422f?q=80&w=1393&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-				className="-mt-24">
+				className="mt-0">
 				<h1 className="text-4xl lg:text-5xl text-center sm:text-left">
 					Transformamos tus ideas en experiencias digitales impactantes
 				</h1>
