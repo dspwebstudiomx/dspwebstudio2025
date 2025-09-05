d932397c27dd353183140d42fdecfbbf8e376266
Daniel Perez <dspwebstudio@gmail.com>
2025-09-05 10:17:44 -0600

    Añadir componente Image con manejo de errores y estilos para mejorar la presentación de imágenes

---
+++ app/components/Images/Image.tsx
@@ -0,0 +1,26 @@
+import placeholder from "../../../public/images/placeholder.png";
+
+type imageProps = {
+	src: string;
+	alt: string;
+	width: number;
+	height: number;
+};
+
+const Image = (props: imageProps) => {
+	return (
+		<img
+			src={props.src}
+			alt={props.alt}
+			width={props.width}
+			height={props.height}
+			srcSet=""
+			className="border-4 border-blue-400 rounded-4xl"
+			onError={(e) => {
+				e.currentTarget.src = placeholder;
+			}}
+		/>
+	);
+};
+
+export default Image;
