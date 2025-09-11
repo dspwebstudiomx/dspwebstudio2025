fb724abd178ef7546b97e86b2cc0cc6303bc6cad
Daniel Perez <dspwebstudio@gmail.com>
2025-09-11 12:32:20 -0600

    Actualizar el componente Hero para ajustar la altura mínima del contenido en diferentes tamaños de pantalla.

--- app/components/Sections/Hero/Hero.tsx
+++ app/components/Sections/Hero/Hero.tsx
@@ -27,7 +27,7 @@ const Hero: React.FC<HeroProps> = ({
 			{/* Contenido */}
 			<div
 				id={`hero-content-${heroId}`}
-				className="container relative flex flex-col p-12 md:px-16 md:py-16 xl:px-0 gap-6 min-h-[100vh] md:min-h-[50vh] lg:min-h-[100vh] xl:min-h-[60vh] text-gray-100 justify-center md:items-start z-10 h-full mx-auto">
+				className="container relative flex flex-col p-12 md:px-16 md:py-16 xl:px-40 gap-6 min-h-[100vh] md:min-h-[50vh] lg:min-h-[100vh] xl:min-h-[80vh] text-gray-100 justify-center md:items-start z-10 h-full mx-auto">
 				{children}
 			</div>
 		</section>
