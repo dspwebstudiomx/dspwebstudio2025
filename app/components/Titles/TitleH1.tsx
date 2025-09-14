c32f0baf35229a5f6a45a782524b76b76829dea1
Daniel Perez <dspwebstudio@gmail.com>
2025-09-14 12:07:32 -0600

    Actualizar el componente TitleH1 para ajustar el tamaño de fuente y centrar el texto en pantallas pequeñas.

--- app/components/Titles/TitleH1.tsx
+++ app/components/Titles/TitleH1.tsx
@@ -6,7 +6,7 @@ type TitleH1Props = {
 
 const TitleH1 = ({ title }: TitleH1Props) => {
 	return (
-		<h1 className="audiowide-regular text-4xl lg:text-5xl text-center sm:text-left">
+		<h1 className="audiowide-regular text-3xl sm:text-4xl lg:text-5xl text-left">
 			{title}
 		</h1>
 	);
